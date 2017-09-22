8e527e5a88b06d898719102bfc79bec4e1789913
Jean-Charles <jeancharles.roger@me.com>
2017-09-22 09:38:05 +0200

    LOW / Adds server logs

--- deploy/start.sh
+++ deploy/start.sh
@@ -5,5 +5,6 @@ LOGFILE=server.log
 
 echo "Starting server"
 
-nohup java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer --port 9300 --project demo.prj & echo $! > $PIDFILE
-
+nohup \
+	java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer --port 9300 --project demo.prj > webroot/server.log 2>&1 & \
+	echo $! > $PIDFILE
