581e2e5ea98d1c8e9df5c6292680d2b316fd97ed
Jean-Charles <jeancharles.roger@me.com>
2017-09-25 23:47:12 +0200

    MEDIUM / Test using setsid

--- deploy/start.sh
+++ deploy/start.sh
@@ -6,6 +6,6 @@ LOGFILE=$PWD/logs/server.log
 echo "Starting server from $PWD"
 cd $PWD
 
-nohup \
-    java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer --port 9300 --project demo.prj > $LOGFILE & \
+setsid \
+    java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer --port 9300 --project demo.prj > $LOGFILE 2>&1 & \
     echo $! > $PIDFILE
