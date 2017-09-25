f1adf1c8306e055c38f320ce6696ad28bba81d0e
Jean-Charles <jeancharles.roger@me.com>
2017-09-25 13:13:35 +0200

    LOW / Adds heap log detection and show

--- webroot/js/logs.ts
+++ webroot/js/logs.ts
@@ -6,13 +6,30 @@ addMdlCssIfNotAlreadyPresent();
 
 let logs = document.getElementById("logs");
 if (logs !== null) {
+  function updateMemory(text: String) {
+    let memorySpan = document.getElementById("memory");
+    if (memorySpan !== null) {
+      let heapMessage = "[HEAP] Total used memory: ";
+      let index = text.lastIndexOf(heapMessage);
+      if (index >= 0) {
+        let bytesIndex = text.indexOf(" bytes", index);
+        let memory = text.substring(index + heapMessage.length, bytesIndex);
+        let memoryMb = parseInt(memory) / 1024 / 1024
+        memorySpan.innerText = memoryMb + " mb";
+      }
+    }
+  }
+
   function updateLogs() {
     let request = new XMLHttpRequest();
     request.open("get", "/logs");
     request.onload = (ev) => {
         console.log("Updating logs from server.log");
         if (request.status >= 200 && request.status < 300) {
-            (<HTMLElement>logs).innerText = request.responseText;
+            let text = request.responseText;
+            (<HTMLElement>logs).innerText = text;
+            updateMemory(text);
+
         }
         poll();
     }
