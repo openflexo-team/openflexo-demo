fdeaf8570148a42068517a11cc4e73252204f4e0
Jean-Charles <jeancharles.roger@me.com>
2017-09-26 08:20:24 +0200

    LOW / Try setsid

--- deploy/start.sh
+++ deploy/start.sh
@@ -5,7 +5,7 @@ LOGFILE=$PWD/logs/server.log
 
 echo "Starting server from $PWD"
 
-nohup \
+setsid \
     java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer \
     --port 9300 --project demo.prj > $LOGFILE 2>&1 &
 
