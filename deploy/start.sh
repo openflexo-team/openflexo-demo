befcb9e9e3f885574e05fb925653a6c359552bbf
Jean-Charles <jeancharles.roger@me.com>
2017-09-26 07:50:55 +0200

    MEDIUM / Separates pid get

--- deploy/start.sh
+++ deploy/start.sh
@@ -4,8 +4,9 @@ PIDFILE=$PWD/server.pid
 LOGFILE=$PWD/logs/server.log
 
 echo "Starting server from $PWD"
-cd $PWD
 
-setsid \
-    java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer --port 9300 --project demo.prj > $LOGFILE 2>&1 & \
-    echo $! > $PIDFILE
+nohup \
+    java -cp `find libs | xargs | sed "s/ /:/g"` org.openflexo.http.server.OpenFlexoServer \
+    --port 9300 --project demo.prj > $LOGFILE 2>&1 &
+
+echo $! > $PIDFILE
